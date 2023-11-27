3d96bc0a2f64e698e2af750c25f190efee1c0ac8
Western1833 <145915895+Western1833@users.noreply.github.com>
2023-11-27 19:57:11 +0200

    create gameService

---
+++ client/src/services/gameService.js
@@ -0,0 +1,13 @@
+const baseUrl = 'http://localhost/3030/jsonstore'
+
+export async function create(gameData){
+    const response = await fetch(`${baseUrl}/games`, {
+        method: 'POST',
+        headers: {'content-type: application/json'},
+        body: JSON.stringify(gameData)
+    });
+
+    const result = await response.json();
+
+    return result;
+}
