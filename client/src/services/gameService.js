e1e50dd5003384dbb4c964a9f57120eaecf9fda3
Western1833 <145915895+Western1833@users.noreply.github.com>
2023-11-27 22:27:36 +0200

    Get all games from server

--- client/src/services/gameService.js
+++ client/src/services/gameService.js
@@ -1,7 +1,7 @@
-const baseUrl = 'http://localhost:3030/jsonstore'
+const baseUrl = 'http://localhost:3030/jsonstore/games';
 
 export async function create(gameData){
-    const response = await fetch(`${baseUrl}/games`, {
+    const response = await fetch(baseUrl, {
         method: 'POST',
         headers: {"content-type": "application/json"},
         body: JSON.stringify(gameData)
@@ -10,4 +10,11 @@ export async function create(gameData){
     const result = await response.json();
 
     return result;
+}
+
+export async function allGames(){
+    const response = await fetch(baseUrl);
+    const result = await response.json();
+
+    return Object.values(result);
 }
