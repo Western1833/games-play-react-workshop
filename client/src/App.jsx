545b9283c185a2f41bd725f1f9e90828f85ddfff
Western1833 <145915895+Western1833@users.noreply.github.com>
2023-11-27 18:37:56 +0200

    Added home view and all games view

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,17 @@
+import GameList from "./components/Game-list/GameList.jsx";
 import Header from "./components/Header/Header.jsx";
+import Home from "./components/Home/Home.jsx";
+import {Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div id="box">
       <Header/>
+
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/games" element={<GameList/>}/>
+      </Routes>
     </div>
   )
 }
