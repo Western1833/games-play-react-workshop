5a9370874b672c41d2497cf2b3fa2375a91fc5b5
Western1833 <145915895+Western1833@users.noreply.github.com>
2023-12-01 19:33:33 +0200

    Make register form controlled

--- client/src/components/Authentication/Register.jsx
+++ client/src/components/Authentication/Register.jsx
@@ -1,19 +1,31 @@
+import { useContext } from "react";
+import AuthContext from "../../contexts/authContext.js";
+import { useForm } from "../../hooks/useFormHook.js";
+
 export default function Register(){
+    const {registerSubmitHandler} = useContext(AuthContext);
+
+    const {values, onChange, onSubmit} = useForm({
+        email: '',
+        password: '',
+        "confirm-password": ''
+    }, registerSubmitHandler)
+
     return(
         <section id="register-page" className="content auth">
-            <form id="register">
+            <form id="register" onSubmit={onSubmit}>
                 <div className="container">
                     <div className="brand-logo"></div>
                     <h1>Register</h1>
 
                     <label htmlFor="email">Email:</label>
-                    <input type="email" id="email" name="email" placeholder="maria@email.com"/>
+                    <input type="email" id="email" name="email" placeholder="maria@email.com" onChange={onChange} value={values.email}/>
 
                     <label htmlFor="pass">Password:</label>
-                    <input type="password" name="password" id="register-password"/>
+                    <input type="password" name="password" id="register-password" onChange={onChange} value={values.password}/>
 
                     <label htmlFor="con-pass">Confirm Password:</label>
-                    <input type="password" name="confirm-password" id="confirm-password"/>
+                    <input type="password" name="confirm-password" id="confirm-password" onChange={onChange} value={values["confirm-password"]}/>
 
                     <input className="btn submit" type="submit" value="Register"/>
 
