15607c86c0e8e45d20635402cf37b5f8b9cafafc
Western1833 <145915895+Western1833@users.noreply.github.com>
2023-12-01 18:17:43 +0200

    Minor changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,19 +4,24 @@ import CreateGame from "./components/CreateGame/CreateGame.jsx";
 import GameList from "./components/Game-list/GameList.jsx";
 import Header from "./components/Header/Header.jsx";
 import Home from "./components/Home/Home.jsx";
-import {Routes, Route} from 'react-router-dom';
+import {Routes, Route, useNavigate} from 'react-router-dom';
 import { paths } from "./utils/apis.js";
 import Details from "./components/Details/Details.jsx";
 import { useState } from "react";
 import AuthContext from "./contexts/authContext.js";
 import * as authService from '../src/services/authService.js';
+import Path from "./paths.js";
 
 function App() {
   const [auth, setAuth] = useState({});
+  const navigate = useNavigate();
 
   const loginSubmitHandler = async (values) => {
     const result = await authService.login(values.email, values.password);
-    console.log(result);
+
+    setAuth(result);
+
+    navigate(Path.Home);
   }
 
   return (
