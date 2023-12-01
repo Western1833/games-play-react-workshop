267f47f64109b748358bc2ae2bc03ebeec68f0ba
Western1833 <145915895+Western1833@users.noreply.github.com>
2023-12-02 00:01:21 +0200

    Changed baseUrl plus error handling for allGames functionality

--- client/src/services/gameService.js
+++ client/src/services/gameService.js
@@ -1,5 +1,6 @@
-const baseUrl = 'http://localhost:3030/jsonstore/games';
 import * as request from '../utils/request.js';
+
+const baseUrl = 'http://localhost:3030/data/games';
 
 export async function create(gameData){
     const result = await request.post(baseUrl, gameData);
@@ -8,9 +9,21 @@ export async function create(gameData){
 }
 
 export async function allGames(){
-    const result = await request.get(baseUrl);
-
-    return Object.values(result);
+    try {
+        const result = await request.get(baseUrl);
+    
+        // Check if the response status is 404
+        if (result && result.code === 404) {
+          throw new Error('Resource not found');
+        }
+    
+        return Object.values(result);
+      } catch (error) {
+        // Handle the error here, you can log it or handle it in another way
+        console.error('Error fetching data:', error.message);
+        // You might return a default value or an empty array in case of error
+        return [];
+      }
 }
 
 export async function getOne(gameId){
